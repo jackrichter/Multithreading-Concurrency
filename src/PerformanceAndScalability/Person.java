b4cfe609cfedbc1b61f562c0e1798610cdb87509
jackrichter <jackrichter1@gmail.com>
2026-03-21 01:42:31 +0100

    Performance and Scalability. Section 9. Immutability.

---
+++ src/PerformanceAndScalability/Person.java
@@ -0,0 +1,21 @@
+package PerformanceAndScalability;
+
+public class Person {
+
+    // Immutable
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
