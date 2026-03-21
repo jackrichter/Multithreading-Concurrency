5afdee51823bea3631afa259cbcc753d71f51852
jackrichter <jackrichter1@gmail.com>
2026-03-22 00:53:30 +0100

    Java Virtual Machine (JVM) and Threads. Section 10. JVM Thread Management.

---
+++ src/JVMAndThreads/CustomTask.java
@@ -0,0 +1,9 @@
+package JVMAndThreads;
+
+public class CustomTask implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Thread Name" + Thread.currentThread().getName() + ", State: " + Thread.currentThread().getState());
+    }
+}
