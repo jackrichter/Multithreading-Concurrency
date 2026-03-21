b392ee99ad7ca2880ea763f1c42881a4c311105f
jackrichter <jackrichter1@gmail.com>
2026-03-21 17:37:10 +0100

    Performance and Scalability. Section 9. Parallel Stream.

---
+++ src/PerformanceAndScalability/ParallelStreamExampleMain.java
@@ -0,0 +1,14 @@
+package PerformanceAndScalability;
+
+import java.util.List;
+
+public class ParallelStreamExampleMain {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.parallelStream().forEach(number -> {
+            System.out.println("Thread: " + Thread.currentThread().getName() + " Number: " + number + ", Square: " + (number * number));
+        });
+    }
+}
